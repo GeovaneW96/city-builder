6473086e8c3e9221eb861dee23d75c3656e87d33
Geovane Wickert <geovanewickert@users.noreply.github.com>
2026-06-18 23:14:37 -0300

    chore: test coverage threshold

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,5 +5,16 @@ export default defineConfig({
     globals: true,
     environment: "node",
     include: ["src/**/*.test.ts"],
+    coverage: {
+      provider: "v8",
+      include: ["src/**/*.ts"],
+      exclude: ["src/**/*.test.ts", "src/main.ts"],
+      thresholds: {
+        statements: 15,
+        branches: 10,
+        functions: 50,
+        lines: 15,
+      },
+    },
   },
 });
